3a60bf650de86d9381989cbafab6165476df6e62
Christopher Tamayo <chriswtamayo@gmail.com>
2023-10-02 01:34:31 -0700

    Fix about me page responsiveness

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,8 +2,8 @@ import headshot from '../assets/headshot.jpg'
 
 export function AboutMe() {
     return (
-        <div className="container pageContainer d-flex align-items-center">
-            <div className="w-75 fs-5 m-3 lh-lg">
+        <div className="container pageContainer d-flex flex-column-reverse flex-lg-row justify-content-center align-items-center">
+            <div className="bio-text fs-5 m-3">
                 <p className="sectionHeading">About Me</p>
                 <p>
                     Senior at Santa Clara University pursuing a B.S. in Computer Science and Engineering with a minor in Mathematics. 
@@ -11,9 +11,7 @@ export function AboutMe() {
                     Looking for challenging opportunities to help me grow both as a person and engineer. 
                 </p>
             </div>
-            <div className="w-25 m-3">
-                <img className="w-100 h-auto rounded-circle" src={headshot} alt="Professional headshot" />
-            </div>
+            <img className="headshot m-3 h-auto rounded-circle" src={headshot} alt="Professional headshot" />
         </div>
     )
 }
