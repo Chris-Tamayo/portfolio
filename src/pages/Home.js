07a895170d10e881f5f21e03b43365037300f09f
Christopher Tamayo <chriswtamayo@gmail.com>
2023-10-02 01:56:25 -0700

    Modified home page width on smaller devices

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 export function Home() {
     return(
-        <div className="pageContainer homeContainer">
-            <div className="d-inline-block fs-5 ls-sm">
+        <div className="pageContainer homeContainer d-flex justify-content-center align-items-center">
+            <div className="d-inline-block ls-sm">
                 <p className="text-start">Hello, World! I'm</p>
-                <p className="fw-semibold fs-2">CHRISTOPHER TAMAYO</p>
+                <p className="full-name fw-semibold">CHRISTOPHER TAMAYO</p>
                 <p className="text-end">Software Engineer</p>
             </div>
         </div>
