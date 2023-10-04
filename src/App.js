6d7b43484b08226dd1e2168bd3a3aa87e0713ab3
Christopher Tamayo <chriswtamayo@gmail.com>
2023-10-03 21:53:26 -0700

    Add fullpage.js scrolling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
+// import { Home } from './pages/Home';
+// import { AboutMe } from './pages/AboutMe';
+// import { Experience } from './pages/Experience';
+// import ReactFullpage from '@fullpage/react-fullpage';
+
+// function App() {
+//   <ReactFullpage
+//     //fullpage options
+//     scrollingSpeed = {1000} /* Options here */
+
+//     render={({ state, fullpageApi }) => {
+//       return (
+//         <ReactFullpage.Wrapper>
+//           <div className='section'><Home /></div>
+//           <div className='section'><AboutMe /></div>
+//           <div className='section'><Experience /></div>
+//         </ReactFullpage.Wrapper>
+//       );
+//     }}
+//   />
+// }
+
+// export default App;
+
+import React from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
 import './assets/global.css';
 import { Home } from './pages/Home';
 import { AboutMe } from './pages/AboutMe';
 import { Experience } from './pages/Experience';
 
-function App() {
-  return (
-    <>
-      <Home />
-      <AboutMe />
-      <Experience />
-    </>
-  );
-}
-
+const App = () => (
+  <ReactFullpage
+    render={({ state, fullpageApi }) => {
+      return (
+        <div>
+          <div className="section"><Home /></div>
+          <div className="section"><AboutMe /></div>
+          <div className="section"><Experience /></div>
+        </div>
+      );
+    }}
+  />
+);
 export default App;
