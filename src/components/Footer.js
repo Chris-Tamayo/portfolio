b0169731d1634003a8d3de60eb05152ebd4be41d
Christopher Tamayo <chriswtamayo@gmail.com>
2023-10-16 00:41:20 -0700

    Modify footer contact links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,35 @@ import linkedin from '../assets/linkedin.png'
 import github from '../assets/github.png'
 import mail from '../assets/mail.png'
 
+// export function Footer() {
+//     return (
+//         <footer className="m-4">
+//             <a href="mailto:chriswtamayo@gmail.com" target="_blank">
+//                 <img className="contactLogo" src={mail} />
+//             </a>
+//             <a href="https://www.linkedin.com/in/christopher-tamayo/" target="_blank">
+//                 <img className="contactLogo" src={linkedin} />
+//             </a>
+//             <a href="https://github.com/Chris-Tamayo" target="_blank">
+//                 <img className="contactLogo" src={github} />
+//             </a>
+//         </footer>
+//     )
+// }
+
 export function Footer() {
     return (
-        <footer className="m-4">
+        <footer className="m-4 fs-6">
             <a href="mailto:chriswtamayo@gmail.com" target="_blank">
-                <img className="contactLogo" src={mail} />
+                Email 
             </a>
+            <span> // </span>
             <a href="https://www.linkedin.com/in/christopher-tamayo/" target="_blank">
-                <img className="contactLogo" src={linkedin} />
+                LinkedIn
             </a>
+            <span> // </span>
             <a href="https://github.com/Chris-Tamayo" target="_blank">
-                <img className="contactLogo" src={github} />
+                 GitHub
             </a>
         </footer>
     )
